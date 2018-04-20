1e032b715fe0272dd6bf439fae2fec3be3342377
Ricky Kennedy <13516105@std.stei.itb.ac.id>
2018-04-20 12:58:09 +0700

    create dictionary data

--- getData/ejson.py
+++ getData/ejson.py
@@ -5,12 +5,30 @@ fo = open("tweets1.txt","r")
 hasil = json.load(fo)
 results = hasil
 
+data=[]
 for x in results :
-    print (x["text"])
-    print (x["user"]["screen_name"])
+    tweets ={'username':'','tweet':'','link':'','profile_image_url':''}
+    
     id = str(x["id"])
-    link = "https://twitter.com/"+x["user"]["screen_name"]+"/status/"+id
-    print (link)
-    linkpicture = (x["user"]["profile_image_url"])
-    print (linkpicture)
+    links = "https://twitter.com/"+x["user"]["screen_name"]+"/status/"+id
+    
+    tweets["username"] = x["user"]["screen_name"] 
+    # print (tweets["username"])
+
+    tweets["tweet"] = x["text"]
+    # print (tweets["tweet"])
+    
+    tweets["link"] = links
+    # print (tweets["link"])
+
+    tweets["profile_image_url"] = x["user"]["profile_image_url"]
+    # print (tweets["profile_image_url"])
+    
+    # print (x["text"])
+    # print (x["user"]["screen_name"])
+    # print (link)
+    # linkpicture = (x["user"]["profile_image_url"])
+    # print (linkpicture)
+    print (tweets)
+    data.append(tweets)
     print ("\n")
